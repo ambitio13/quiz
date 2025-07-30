17bc60608e7daef9ca1f8ef0c928739df5e03f4e
coldNightawa <2367683048@qq.com>
2025-07-30 20:42:49 +0800

    数据库连接

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 DB = {
     "user":     os.getenv("DB_USER", "root"),
-    "password": os.getenv("DB_PASSWORD", "123456"),
+    "password": os.getenv("DB_PASSWORD", "8520456qwebq"),
     "host":     os.getenv("DB_HOST", "localhost"),
     "database": os.getenv("DB_NAME", "interactive_quiz"),
     "port":     3306,
